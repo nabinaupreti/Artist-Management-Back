ba93d938bb11b337579a18a1a6b28973fea9c254
Nabina Upreti <nabina419@gmail.com>
2025-03-27 17:41:30 +0545

    CURD operation for artist

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -14,22 +14,50 @@ class SignupView(APIView):
             hashed_password = make_password(data["password"])
 
             user_id = UserModel.create_user(
-                data["first_name"], data["last_name"], data["email"], hashed_password,
-                data.get("phone"), data.get("dob"), data["gender"], data.get("address"), data["role_type"]
+                data["first_name"],
+                data["last_name"],
+                data["email"],
+                hashed_password,
+                data.get("phone"),
+                data.get("dob"),
+                data["gender"],
+                data.get("address"),
+                data["role_type"],
             )
 
-            return Response({"message": "User created successfully", "user_id": user_id}, status=status.HTTP_201_CREATED)
+            return Response(
+                {"message": "User created successfully", "user_id": user_id},
+                status=status.HTTP_201_CREATED,
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginView(APIView):
     def post(self, request):
+        """
+        Handles user login.
+        Only allows login for approved users.
+        """
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid():
             data = serializer.validated_data
             user = UserModel.get_user_by_email(data["email"])
 
-            if user and check_password(data["password"], user[4]):  # user[4] is password
-                access_token, refresh_token = JWTHandler.generate_tokens(user[0])  # user[0] is id
-                return Response({"access_token": access_token, "refresh_token": refresh_token}, status=status.HTTP_200_OK)
+            if user and check_password(data["password"], user["password"]):
+                
+
+                # Generate tokens for approved users
+                access_token, refresh_token = JWTHandler.generate_tokens(user["id"])
+                return Response({
+                    "message": "Login successful",
+                    "user": {
+                        "id": user["id"],
+                        "email": user["email"],
+                        "first_name": user["first_name"],
+                        "last_name": user["last_name"],
+                        "role_type": user["role"]
+                    },
+                    "access_token": access_token,
+                    "refresh_token": refresh_token
+                }, status=status.HTTP_200_OK)
 
         return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
